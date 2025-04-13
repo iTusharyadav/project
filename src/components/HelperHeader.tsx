a66c681002b8c42e95ed5b059ff122a440fb8229
Tushar Yadav <tushar43588@gmail.com>
2025-04-13 19:39:07 +0530

    Update HelperHeader.tsx

--- src/components/HelperHeader.tsx
+++ src/components/HelperHeader.tsx
@@ -1,3 +1,54 @@
+// import { Button } from "./ui/button";
+// import { Share2, Save } from "lucide-react";
+// import {
+//     Select,
+//     SelectContent,
+//     SelectItem,
+//     SelectTrigger,
+//     SelectValue,
+// } from "@/components/ui/select"
+// import { useDispatch, useSelector } from "react-redux";
+// // import { compilerSliceStateType, updateCurrentLanguage } from "@/redux/slices/compilerSlice";
+// import { CompilerSliceStateType, updateCurrentLanguage } from "@/redux/slices/compilerSlice";
+// import { RootState } from "@/redux/store";
+
+// export default function HelperHeader() {
+//     const dispatch = useDispatch();
+//     const currentLanguage = useSelector((state: RootState) => state.compilerSlice.currentLanguage);
+//     return (
+//         <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
+//             <div className="__btn_container flex gap-1">
+//                 <Button className="flex justify-center items-center gap-1">
+//                     <Save size={16} />
+//                 </Button>
+//                 <Button className="flex justify-center items-center gap-1" variant="secondary">
+//                     <Share2 size={16} />
+//                     {/* Share */}
+//                 </Button>
+//             </div>
+//             <div className="_tab_switcher flex justify-center items-center gap-1">
+//                 <small>Current Language:</small>
+//                 <Select defaultValue={currentLanguage}
+//                     onValueChange={(value) => dispatch(updateCurrentLanguage(value as compilerSliceStateType["currentLanguage"]))}>
+
+
+//                     <SelectTrigger className="w-[120px] bg-gray-800 outline-none focus:ring-0">
+//                         <SelectValue />
+//                     </SelectTrigger>
+//                     <SelectContent>
+//                         <SelectItem value="html">Html</SelectItem>
+//                         <SelectItem value="css">CSS</SelectItem>
+//                         <SelectItem value="javascript">JavaScript</SelectItem>
+//                     </SelectContent>
+//                 </Select>
+//             </div>
+//         </div >
+//     )
+// }
+
+
+
+
 import { Button } from "./ui/button";
 import { Share2, Save } from "lucide-react";
 import {
@@ -6,15 +57,17 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
 import { useDispatch, useSelector } from "react-redux";
-// import { compilerSliceStateType, updateCurrentLanguage } from "@/redux/slices/compilerSlice";
 import { CompilerSliceStateType, updateCurrentLanguage } from "@/redux/slices/compilerSlice";
 import { RootState } from "@/redux/store";
 
 export default function HelperHeader() {
     const dispatch = useDispatch();
-    const currentLanguage = useSelector((state: RootState) => state.compilerSlice.currentLanguage);
+    const currentLanguage = useSelector(
+        (state: RootState) => state.compilerSlice.currentLanguage
+    );
+
     return (
         <div className="__helper_header h-[50px] bg-black text-white p-2 flex justify-between items-center">
             <div className="__btn_container flex gap-1">
@@ -23,15 +76,20 @@ export default function HelperHeader() {
                 </Button>
                 <Button className="flex justify-center items-center gap-1" variant="secondary">
                     <Share2 size={16} />
-                    {/* Share */}
                 </Button>
             </div>
             <div className="_tab_switcher flex justify-center items-center gap-1">
                 <small>Current Language:</small>
-                <Select defaultValue={currentLanguage}
-                    onValueChange={(value) => dispatch(updateCurrentLanguage(value as compilerSliceStateType["currentLanguage"]))}>
-
-
+                <Select
+                    defaultValue={currentLanguage}
+                    onValueChange={(value) =>
+                        dispatch(
+                            updateCurrentLanguage(
+                                value as CompilerSliceStateType["currentLanguage"]
+                            )
+                        )
+                    }
+                >
                     <SelectTrigger className="w-[120px] bg-gray-800 outline-none focus:ring-0">
                         <SelectValue />
                     </SelectTrigger>
@@ -42,6 +100,6 @@ export default function HelperHeader() {
                     </SelectContent>
                 </Select>
             </div>
-        </div >
-    )
+        </div>
+    );
 }
